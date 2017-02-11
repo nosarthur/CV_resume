7e99e812b544af6b1d447dc83bc05818e09e0c4d
Dong Zhou <zhou.dong@gmail.com>
2017-02-11 15:37:54 -0500

    update

--- keywords/makeWordCloud.py
+++ keywords/makeWordCloud.py
@@ -14,7 +14,8 @@ def make_wordcloud(filename, titles):
     for t in titles:
         counts.update(t)
     pairs = [(w, c) for (w, c) in counts.items() if c > 1]
-    wordcloud = WordCloud(background_color=None, mode='RGBA',random_state=42).generate_from_frequencies(pairs)
+    wordcloud = WordCloud(background_color=None, mode='RGBA',
+                          random_state=42).generate_from_frequencies(pairs)
     print counts
     plt.figure()
     plt.imshow(wordcloud)
